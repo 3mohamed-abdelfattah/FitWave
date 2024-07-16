d5e89b7f7751e32875dc5d0c03b04b688b0fec72
MohamedMohamed <m01126165016@icloud.com>
2024-07-16 04:25:43 +0300

    Dashboard Page

--- app/src/main/java/kmp/project/fitwave/MainActivity.kt
+++ app/src/main/java/kmp/project/fitwave/MainActivity.kt
@@ -3,18 +3,19 @@ package kmp.project.fitwave
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kmp.project.fitwave.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
     }
 }
