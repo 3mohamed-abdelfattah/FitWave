6f2836aa7d26c15dd6e210dc49f41200b44ab177
MohamedMohamed <m01126165016@icloud.com>
2024-07-18 05:19:15 +0300

    Update Workout Activity

--- app/src/main/java/kmp/project/fitwave/activity/WorkoutActivity.kt
+++ app/src/main/java/kmp/project/fitwave/activity/WorkoutActivity.kt
@@ -1,21 +1,58 @@
 package kmp.project.fitwave.activity
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import kmp.project.fitwave.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import kmp.project.fitwave.adapter.LessonAdapter
+import kmp.project.fitwave.databinding.ActivityWorkoutBinding
+import kmp.project.fitwave.domain.Workout
 
 class WorkoutActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWorkoutBinding
+    private var workout: Workout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityWorkoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_workout)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        getObject()
+        setVariable()
+    }
+
+    private fun getObject() {
+        workout = intent.getSerializableExtra("object") as? Workout
+    }
+
+    private fun setVariable() {
+        workout?.let { workout ->
+            val resId = resources.getIdentifier(workout.picPath, "drawable", packageName)
+            Glide.with(this)
+                .load(resId)
+                .into(binding.pic)
+
+            binding.backBtn.setOnClickListener { finish() }
+            binding.titleText.text = workout.title
+            binding.exerciseText.text = "${workout.lessons.size} Exercise"
+            binding.kcalText.text = "${workout.kcal} Kcal"
+            binding.durationText.text = workout.durationAll
+            binding.descrptionText.text = workout.description
+
+            binding.view3.layoutManager =
+                LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+            binding.view3.adapter = LessonAdapter(workout.lessons)
         }
     }
 }
